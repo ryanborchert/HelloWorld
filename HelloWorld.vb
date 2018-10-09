1d40e747e5e416d462904374108578ddb0af4f24
ryanborchert <ryan.borchert@gmail.com>
2018-10-09 17:25:54 -0400

    Add Hello World application for visual basic

---
+++ HelloWorld.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Namespace HelloWorld
+    Public Module Program
+        Public Sub Main(args() As string)
+            'Your code goes here
+            Console.WriteLine("Hello world")
+        End Sub
+    End Module
+End Namespace
